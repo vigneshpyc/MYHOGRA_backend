ab6f3a34d9e1f3f0f5e638384ecfee675c22a4e1
vigneshpyc <jeevaviki1609@gmail.com>
2026-04-18 19:56:30 +0530

    production commit v2

--- app/core/security.py
+++ app/core/security.py
@@ -4,7 +4,6 @@ from app.core.config import SECRET_KEY, ALGORITHM
 
 def create_token(data:dict, expires_delta: int):
     to_encode = data.copy()
-    print(type(expires_delta))
     expire = datetime.utcnow() + timedelta(minutes=expires_delta)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
